7260c856c17641ea770432dbda81511ad5220146
Christian Kemper <ckemper@gmail.com>
2026-04-29 06:54:22 -0700

    fix(erfa): rewrite 2000B wrappers to mirror 2000A call chain
    Replace the incorrect populate_astrom_2000b/atci00b/atic00b helpers
    with proper eraApci00b/eraAtci00b/eraAtic00b wrappers that mirror
    eraApci13/eraAtci13/eraAtic13 exactly, substituting only:
    - eraPnm06a -> eraPnm00b (77-term nutation)
    - eraS06    -> eraS00    (IAU 2000 CIO locator)
    
    Fixes three bugs in the old implementation:
    - eraEe00b (equation of equinoxes) used instead of eraEors (equation
      of origins), causing wrong RA conversions
    - eraApci called with x=y=s=0 then BPN matrix force-overwritten
    - eraAtciqz (zero proper motion) used instead of eraAtciq

--- libs/indicore/ErfaEngine.cpp
+++ libs/indicore/ErfaEngine.cpp
@@ -9,45 +9,31 @@
 #include <memory>
 
 #ifdef HAVE_ERFA
-/**
- * Internal helper to build an eraASTROM context using IAU 2000B.
- */
-static void populate_astrom_2000b(double date1, double date2, eraASTROM *astrom, double *eo) {
-    double rbpn[3][3], pvh[2][3], pvb[2][3];
-    
-    // 1. IAU 2000B BPN and Equation of Origins
-    eraPnm00b(date1, date2, rbpn);
-    *eo = eraEe00b(date1, date2);
-
-    // 2. Convert Equinox-based BPN to CIO-based (CIRS) BPN
-    // R_cio = R_z(-eo) * R_equinox
-    double rz[3][3], rbpn_cio[3][3];
-    eraIr(rz);
-    eraRz(-(*eo), rz);
-    eraRxr(rz, rbpn, rbpn_cio);
-    
-    // 3. Populate context from Earth ephemeris
-    eraEpv00(date1, date2, pvh, pvb);
-    eraApci(date1, date2, pvb, pvh[0], 0, 0, 0, astrom);
-    
-    // 4. Force context to use the 2000B matrix
-    for (int i=0; i<3; i++) for (int j=0; j<3; j++) astrom->bpn[i][j] = rbpn_cio[i][j];
+static void eraApci00b(double date1, double date2, eraASTROM *astrom, double *eo)
+{
+    double ehpv[2][3], ebpv[2][3], r[3][3], x, y, s;
+    eraEpv00(date1, date2, ehpv, ebpv);
+    eraPnm00b(date1, date2, r);
+    eraBpn2xy(r, &x, &y);
+    s = eraS00(date1, date2, x, y);
+    eraApci(date1, date2, ebpv, ehpv[0], x, y, s, astrom);
+    *eo = eraEors(r, s);
 }
 
-static void atci00b(double rc, double dc, double date1, double date2,
-                    double *ri, double *di, double *eo) {
+static void eraAtci00b(double rc, double dc, double pr, double pd, double px, double rv,
+                       double date1, double date2, double *ri, double *di, double *eo)
+{
     eraASTROM astrom;
-    populate_astrom_2000b(date1, date2, &astrom, eo);
-    eraAtciqz(rc, dc, &astrom, ri, di);
+    eraApci00b(date1, date2, &astrom, eo);
+    eraAtciq(rc, dc, pr, pd, px, rv, &astrom, ri, di);
 }
 
-static void atic00b(double ri, double di, double date1, double date2,
-                    double *rc, double *dc) {
+static void eraAtic00b(double ri, double di, double date1, double date2,
+                       double *rc, double *dc, double *eo)
+{
     eraASTROM astrom;
-    double eo;
-    populate_astrom_2000b(date1, date2, &astrom, &eo);
-    // Convert Equinox RA to CIO RA (RI = RA_equinox + eo)
-    eraAticq(eraAnp(ri + eo), di, &astrom, rc, dc);
+    eraApci00b(date1, date2, &astrom, eo);
+    eraAticq(ri, di, &astrom, rc, dc);
 }
 #endif
 
@@ -111,7 +97,7 @@ public:
         double ri = HOURS_TO_RAD(j2000->rightascension);
         double di = DEG_TO_RAD(j2000->declination);
         double rc, dc, eo;
-        atci00b(ri, di, utc1, utc2, &rc, &dc, &eo);
+        eraAtci00b(ri, di, 0, 0, 0, 0, utc1, utc2, &rc, &dc, &eo);
         jnow->rightascension = RAD_TO_HOURS(eraAnp(rc - eo));
         jnow->declination = RAD_TO_DEG(dc);
 #else
@@ -122,12 +108,15 @@ public:
     void ObservedToJ2000(INDI::IEquatorialCoordinates *jnow, double jd, INDI::IEquatorialCoordinates *j2000) override {
 #ifdef HAVE_ERFA
         double utc1 = std::floor(jd) + 0.5, utc2 = jd - utc1;
-        double ri = HOURS_TO_RAD(jnow->rightascension);
-        double di = DEG_TO_RAD(jnow->declination);
-        double rc, dc;
-        atic00b(ri, di, utc1, utc2, &rc, &dc);
-        j2000->rightascension = RAD_TO_HOURS(eraAnp(rc));
-        j2000->declination = RAD_TO_DEG(dc);
+        double ra_jnow_rad = HOURS_TO_RAD(jnow->rightascension);
+        double dec_jnow_rad = DEG_TO_RAD(jnow->declination);
+        eraASTROM astrom;
+        double eo;
+        eraApci00b(utc1, utc2, &astrom, &eo);
+        double ri, di;
+        eraAtic00b(eraAnp(ra_jnow_rad + eo), dec_jnow_rad, utc1, utc2, &ri, &di, &eo);
+        j2000->rightascension = RAD_TO_HOURS(eraAnp(ri));
+        j2000->declination = RAD_TO_DEG(di);
 #else
         INDI_UNUSED(jnow); INDI_UNUSED(jd); INDI_UNUSED(j2000);
 #endif
@@ -138,7 +127,7 @@ public:
         double utc1 = std::floor(JD) + 0.5, utc2 = JD - utc1;
         eraASTROM astrom;
         double eo;
-        populate_astrom_2000b(utc1, utc2, &astrom, &eo);
+        eraApci00b(utc1, utc2, &astrom, &eo);
         double ra_rad = HOURS_TO_RAD(object->rightascension);
         double dec_rad = DEG_TO_RAD(object->declination);
         double ra_cirs = eraAnp(ra_rad + eo);
